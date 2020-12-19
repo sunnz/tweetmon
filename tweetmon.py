2a51784ab0733f3e3380c296875e8485157fb95b
sunnz <sunnzy@gmail.com>
2020-12-19 21:21:38 +1100

    Added scheduler to display new tweets every 10 minutes

--- tweetmon.py
+++ tweetmon.py
@@ -10,12 +10,29 @@ Options:
 
 from docopt import docopt
 from scrapper import scrape_recent_tweets
+import schedule
+import time
+import datetime
 
-import json
 
+def tweetmon(username):
+    print(f'Searching for five most recent tweets from {username}')
+    tweets = scrape_recent_tweets(username)
+    print_tweets(tweets)
+    print(datetime.datetime.now().isoformat())
+
+def print_tweets(tweets):
+    print('=================================')
+    print("\n----------------\n".join(tweets))
+    print('=================================')
 
 if __name__ == "__main__":
     args = docopt(__doc__)
     username = args['<handle>']
-    tweets = scrape_recent_tweets(username)
-    print(json.dumps(tweets, indent=2))
+    schedule.every(10).minutes.do(tweetmon, username=username)
+    # run first one straight away before running every ten minutes
+    tweetmon(username)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
