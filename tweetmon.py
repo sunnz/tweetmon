6fc923ad4d8c52e4c9586472dc4909862e2cbbb8
sunnz <sunnzy@gmail.com>
2020-12-19 22:59:12 +1100

    Added simple API (flask) to dump all tweets collected so far in JSON format

--- tweetmon.py
+++ tweetmon.py
@@ -2,29 +2,51 @@
 
 """
 Usage:
-    tweetmon.py [-w] <handle>
+    tweetmon.py [-s] <handle>
 
 Options:
-    -w            Number of seconds to wait for javascript to load tweets.
+  -h --help  Show this screen.
+  -s         Starts API service to dump all tweets collected so far in JSON format.
+
 """
 
 from docopt import docopt
+from flask import Flask
+from flask import jsonify
+from threading import Thread
 from scrapper import scrape_recent_tweets
 import schedule
 import time
 import datetime
 
 
+# prepare flask and thread to run a simple api
+app = Flask(__name__)
+collected_tweets = []
+app_thread = None
+
 def tweetmon(username):
+    """Calls scrapper module, continously print new tweets and updates shared memory for Simple API"""
     print(f'Searching for five most recent tweets from {username}')
     tweets = scrape_recent_tweets(username)
     print_tweets(tweets)
-    print(datetime.datetime.now().isoformat())
+    collected_tweets.clear()
+    collected_tweets.extend(tweets)
 
 def print_tweets(tweets):
     print('=================================')
     print("\n----------------\n".join(tweets))
     print('=================================')
+    print(datetime.datetime.now().isoformat())
+
+def app_run():
+    """Function to run flask app in a separate thread, reads collected_tweets"""
+    app.run()
+
+@app.route('/', methods=['GET'])
+def get_collected_tweets():
+    """Reads tweets collected so far in JSON format"""
+    return jsonify(collected_tweets)
 
 if __name__ == "__main__":
     args = docopt(__doc__)
@@ -32,7 +54,17 @@ if __name__ == "__main__":
     schedule.every(10).minutes.do(tweetmon, username=username)
     # run first one straight away before running every ten minutes
     tweetmon(username)
+    if args['-s']:
+        # creates separate thread for running the flask app
+        app_thread = Thread(target=app_run)
+        app_thread.start()
 
     while True:
-        schedule.run_pending()
-        time.sleep(1)
+        try:
+            schedule.run_pending()
+            time.sleep(1)
+        except:
+            break
+
+    if app_thread is not None:
+        app_thread.join()
